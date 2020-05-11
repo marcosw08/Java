e7a957a0d1bdc0db6474eec630cefc520db2e03c
marc <mr.marc.oswald@gmail.com>
2020-05-11 18:15:31 +0200

    So! Wir haben ein Programm angefangen dass in der Konsole stattfindet. Man kann auch Text eingeben, etc.

--- src/ch/marc/valentin/datenbank/experiment/Hallo.java
+++ src/ch/marc/valentin/datenbank/experiment/Hallo.java
@@ -1,8 +1,24 @@
 package ch.marc.valentin.datenbank.experiment;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Hallo {
 
-	public static void main(String[] args) {
-		System.out.println("Hallo, Marc und Valentin");
+	public static void main(String[] args) throws IOException {
+		String name = liesVonDerKonsole("Wie heisst du? ");
+		System.out.println("Hallo, " + name.toUpperCase() + ", du Held!");
+	}
+	
+	private static String liesVonDerKonsole(String meldung) throws IOException {
+		// Geht leider nur im Terminal, nicht in Eclipse:
+		//
+		// String name = System.console().readLine(meldung);
+		// 
+		// System.in und System.out geht immer:
+		System.out.print(meldung);
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		return bufferedReader.readLine();
 	}
 }
