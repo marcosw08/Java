213795bfb929bebceaffc6f1b9c9fef277bc883b
marc <marc@macbook-pro.home>
2020-05-31 17:58:36 +0200

    Das neue Programm erstellt. Das Gästebuch

---
+++ src/ch/marc/valentin/datenbank/experiment/Gaestebuch.java
@@ -0,0 +1,14 @@
+package ch.marc.valentin.datenbank.experiment;
+
+public class Gaestebuch {
+
+	public static void main(String[] args) {
+		System.out.println("Hallo, was wollen Sie tun?");
+		System.out.println("A - Einträge anzeigen");
+		System.out.println("B - Eintrag hinzufügen");
+		System.out.println("C - Eintrag löschen");
+		System.out.println("Beenden");
+
+	}
+
+}
