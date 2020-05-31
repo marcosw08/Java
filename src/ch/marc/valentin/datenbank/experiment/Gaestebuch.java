88f17ab14550b0bb61199096808e822c80a34837
Valentin Perrez <valentin.perrez@bluewin.ch>
2020-05-31 18:22:44 +0200

    Gaestebuch erweitert

--- src/ch/marc/valentin/datenbank/experiment/Gaestebuch.java
+++ src/ch/marc/valentin/datenbank/experiment/Gaestebuch.java
@@ -1,14 +1,43 @@
 package ch.marc.valentin.datenbank.experiment;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Gaestebuch {
 
-	public static void main(String[] args) {
-		System.out.println("Hallo, was wollen Sie tun?");
-		System.out.println("A - Einträge anzeigen");
-		System.out.println("B - Eintrag hinzufügen");
-		System.out.println("C - Eintrag löschen");
-		System.out.println("D - Beenden");
-
+	public static void main(String[] args) throws IOException {
+		String auswahl;
+		do {
+			System.out.println("Hallo, was wollen Sie tun?");
+			System.out.println("A - Einträge anzeigen");
+			System.out.println("B - Eintrag hinzufügen");
+			System.out.println("C - Eintrag löschen");
+			System.out.println("D - Beenden");
+			auswahl =liesVonDerKonsole("Buchstabe eintippen: ");
+			if(auswahl.equals("A")) {
+				System.out.println("Einträge anzeigen");
+				
+			}
+			if(auswahl.equals("B")) {
+				System.out.println("Eintrag hinzufügen");
+				
+			}
+			if(auswahl.equals("C")) {
+				System.out.println("Eintrag löschen");
+				
+			}
+		} while(!auswahl.equals("D"));
+	}
+	private static String liesVonDerKonsole(String meldung) throws IOException {
+		// Geht leider nur im Terminal, nicht in Eclipse:
+		//
+		// String name = System.console().readLine(meldung);
+		// 
+		// System.in und System.out geht immer:
+		System.out.print(meldung);
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		return bufferedReader.readLine();
 	}
 
 }
